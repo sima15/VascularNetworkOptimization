a21104b3ca912b129a4827f2830e51d062f60ed9
Sima Mehri <s.mehry.89@gmail.com>
2017-03-04 20:22:03 -0700

    removed unnecessary files

--- examples/src/iDynoOptimizer/Results/Feature/Names/FeatureNameSimulation.java
+++
@@ -1,9 +0,0 @@
-package iDynoOptimizer.Results.Feature.Names;
-
-/**
- * Created by Chris Johnson on 4/17/2015.
- */
-public enum FeatureNameSimulation implements FeatureName {
-
-    velocitySimulation
-}
